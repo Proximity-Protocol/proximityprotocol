6b2cb39efbee03a6b9783d1d148c58d4baf819ab
apinanyogaratnam <apinan@ontopical.com>
2023-03-31 19:00:13 -0400

    feat: progress

---
+++ Makefile
@@ -0,0 +1,6 @@
+compile:
+	npx hardhat compile
+
+deploy:
+	# npx hardhat deploy --network ganache
+	npx hardhat deploy --network hardhat
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,57 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { task } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/types";
+import "@nomiclabs/hardhat-ethers";
+import "@openzeppelin/hardhat-upgrades";
+import 'hardhat-contract-sizer';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  networks: {
+    ganache: {
+      url: "http://127.0.0.1:8545",
+      gas: 1000000000000000,
+    },
+    hardhat: {
+      accounts: {
+        mnemonic: process.env.MNEMONIC
+      },
+      gas: 8000000,
+    },
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: true,
+    disambiguatePaths: false,
+  },
 };
+
+task("deploy", "Deploy the ProximityProtocol and ProximityProtocolGlobal contracts")
+  .setAction(async (args, hre) => {
+    const { ethers, upgrades } = hre;
+    const [deployer] = await ethers.getSigners();
+
+    console.log("Deploying contracts with account:", deployer.address);
+
+    const ProximityProtocol = await ethers.getContractFactory("ProximityProtocol");
+    const ProximityProtocolProxy = await upgrades.deployProxy(ProximityProtocol, ["https://example.com"], { initializer: "initialize" });
+    await ProximityProtocolProxy.deployed();
+    console.log("ProximityProtocol deployed at:", ProximityProtocolProxy.address);
+
+    // const ProximityProtocolGlobal = await ethers.getContractFactory("ProximityProtocolGlobal");
+    // const ProximityProtocolGlobalProxy = await upgrades.deployProxy(ProximityProtocolGlobal, ["https://example.com", ProximityProtocolProxy.address], { initializer: "initialize" });
+    // await ProximityProtocolGlobalProxy.deployed();
+    // console.log("ProximityProtocolGlobal deployed at:", ProximityProtocolGlobalProxy.address);
+  });
 
 export default config;
